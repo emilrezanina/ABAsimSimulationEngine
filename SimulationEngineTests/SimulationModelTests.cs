821d8ebe223b72deb450557c456496b62d7c084b
emilrezanina <rezanina.emil@gmail.com>
2015-01-22 01:56:58 +0100

    UPDATE: Deleted folder with unused projects.

---
+++ SimulationEngineTests/SimulationModelTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace SimulationEngineTests
+{
+    class SimulationModelTests
+    {
+        //[Fact]
+        //void CheckCountOfAgent()
+        //{
+        //    //kontrola pri vlozeni agentu do simulacniho modelu, ze jich tam je ...
+        //    //pouziti spise Theory
+        //}
+    }
+}
